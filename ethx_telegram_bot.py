f09a38e9c6a38a8cd4313688b56b094fec9f1151
Mithun Madhusoodanan <cybermithun@gmail.com>
2018-02-28 00:18:44 +0530

    added bitcoin and litcoin prices

--- ethx_telegram_bot.py
+++ ethx_telegram_bot.py
@@ -9,17 +9,23 @@ from scrapy import Selector
 TOKEN = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
 
 
-def sell_price():
+def eth_price():
     r = requests.get('https://ethx.in')
     sel = Selector(text=r.content)
-    sell_price = sel.xpath('//*[@id="page-header"]/div[1]/div[2]/ul/li[2]/a/text()').extract()[0]
-    return sell_price
+    eth_price = sel.xpath('//*[@id="page-header"]/div[1]/div[2]/ul/li[2]/a/text()').extract()[0]
+    return eth_price
 
-def buy_price():
+def bit_price():
     r = requests.get('https://ethx.in')
     sel = Selector(text=r.content)
-    buy_price = sel.xpath('//*[@id="page-header"]/div[1]/div[2]/ul/li[1]/a/text()').extract()[0]
-    return buy_price
+    bit_price = sel.xpath('//*[@id="page-header"]/div[1]/div[2]/ul/li[1]/a/text()').extract()[0]
+    return bit_price
+
+def ltc_price():
+    r = requests.get('https://ethx.in')
+    sel = Selector(text=r.content)
+    ltc_price = sel.xpath('//*[@id="page-header"]/div[1]/div[2]/ul/li[3]/a/text()').extract()[0]
+    return ltc_price
 
 def on_chat_message(msg):
     content_type, chat_type, chat_id = telepot.glance(msg)
@@ -32,10 +38,12 @@ def on_chat_message(msg):
     if message=="/start":
         bot.sendMessage(chat_id, 'Get Etherium prices from ETHX.IN', reply_markup=markup)
     
-    if message=="Buy Price":
-        bot.sendMessage(chat_id, buy_price(), reply_markup=markup)
-    if message=="Sell Price":
-        bot.sendMessage(chat_id, sell_price(), reply_markup=markup)
+    if message=="Bitcoin Price":
+        bot.sendMessage(chat_id, bit_price(), reply_markup=markup)
+    if message=="Etherium Price":
+        bot.sendMessage(chat_id, eth_price(), reply_markup=markup)
+    if message=="Litcoin Price":
+        bot.sendMessage(chat_id, ltc_price(), reply_markup=markup)
 
 
 def on_callback_query(msg):
